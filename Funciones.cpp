f582e7f64b198729d628291db1a8f77eebf8e537
Adriana240292 <jcatalan@miumg.edu.gt>
2025-03-29 18:47:18 -0600

    Funciones

--- Funciones.cpp
+++ Funciones.cpp
@@ -8,16 +8,50 @@ using namespace std;
 // crean una funcion o un metodo = y lo manda a llamar 10 veces = 80 lineas de codigo y 10 llamadas a funciones
 
 // funcion que sume 2 valores
-int suma(int num1,int num2){
+
+/*int suma(int num1,int num2){
 	int resultado = 0;
 	resultado = num1+num2;
 	return resultado;
 }
+/*int resta(int num1,int num2){
+	int resultado = 0;
+	resultado = num1-num2;
+	return resultado;
+}*/
+
+/*int suma(int num1,int num2){
+	int resultado = 0;
+	resultado = num1+num2;
+	return resultado;
+}
+
+int suma(int num1,int num2, int num3){
+	int resultado = 0;
+	resultado = num1+num2+num3;
+	return resultado;*/
+
+
+//Función que sume dos valores
+int suma(int num1,int num2, int num3){
+	int resultado = 0;
+	resultado = num1+num2+num3;
+	return resultado;
+}
+// Metodo y envio de parametros por valor y referencia
+void suma(int &num1,int &num2){
+	int resultado = 0;
+	num1+=1; // 11
+	num2+=1; // 21
+	resultado = num1+num2; // 32
+	cout<<resultado<<endl;
+}
+
 main(){
-	
-cout<<suma(10,20)<<endl;
-cout<<suma(50,70)<<endl;
-cout<<suma(90,10)<<endl;	
+int a=10,b = 20;	
+suma(a,b);
+cout<<"a: "<<a<<endl;
+cout<<"b: "<<b<<endl;
 
 	
 	system("pause");
